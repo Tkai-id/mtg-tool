254c563fb1ae83785a5fd8c0968bd16ddb6a308f
Tkai-id <kai.tanaka@identify.co.jp>
2026-03-23 15:12:21 +0900

    Update clients.js

--- api/clients.js
+++ api/clients.js
@@ -1,1 +1,6 @@
 
+export default function handler(req, res) {
+  const clientList = process.env.CLIENT_LIST || '';
+  const clients = clientList.split('\n').map(s => s.trim()).filter(s => s.length > 0);
+  res.status(200).json({ clients });
+}
