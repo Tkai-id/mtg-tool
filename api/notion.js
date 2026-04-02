1ec6fe8df6afef5de9d8702d1b28066333e9d1c4
Tkai-id <kai.tanaka@identify.co.jp>
2026-04-02 17:10:20 +0900

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,18 +1,39 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
-  const { title, client, date, content } = req.body;
+  const { title, client, date, content, transcript } = req.body;
 
   const notionKey = process.env.NOTION_API_KEY;
   const databaseId = process.env.NOTION_DB_ID;
 
-  const paragraphs = content.split('\n').map(line => ({
+  const summaryParagraphs = content.split('\n').map(line => ({
     object: 'block',
     type: 'paragraph',
     paragraph: {
       rich_text: [{ type: 'text', text: { content: line } }]
     }
   }));
+
+  // 文字起こしを折りたたみブロックに
+  const transcriptLines = transcript.split('\n').map(line => ({
+    object: 'block',
+    type: 'paragraph',
+    paragraph: {
+      rich_text: [{ type: 'text', text: { content: line } }]
+    }
+  }));
+
+  const children = [
+    ...summaryParagraphs,
+    {
+      object: 'block',
+      type: 'toggle',
+      toggle: {
+        rich_text: [{ type: 'text', text: { content: '▼ 元の文字起こし' } }],
+        children: transcriptLines.slice(0, 100)
+      }
+    }
+  ];
 
   const response = await fetch('https://api.notion.com/v1/pages', {
     method: 'POST',
@@ -25,10 +46,10 @@ export default async function handler(req, res) {
       parent: { database_id: databaseId },
       properties: {
         '名前': { title: [{ text: { content: title } }] },
-        クライアント名: { rich_text: [{ text: { content: client } }] },
-        MTG日: { date: { start: date } },
+        'クライアント名': { rich_text: [{ text: { content: client } }] },
+        'MTG日': { date: { start: date } },
       },
-      children: paragraphs,
+      children,
     })
   });
 
